fd96e0f5cd157b65e89d3eebdf3666dcd81ffda8
mzhao94 <miraezhao@gmail.com>
2016-07-07 10:36:46 -0700

    Starting working on grid for maximization

---
+++ maximization.py
@@ -0,0 +1,44 @@
+from enum import Enum
+import possible_effort_splits as pes
+
+class TimePeriod(Enum):
+	tminusone = 0
+	t = 1
+	tplusone = 2
+
+
+# Function: returns_for_young
+# Calculate a return for a specific effort split of the maximizing scientist of interest in time period t
+# Return: a return for that scientist in time period t
+def returns_for_young(effort_on_tminusone, effort_on_t):
+
+
+# Function: returns_for_old
+# Calculate the returns for specific effort split of the maximizing scientist of interest in time period t+1
+# Return: a return for that scientist in time period t+1
+def returns_for_old(effort_on_tminusone, effort_on_t, effort_on_tplusone):
+
+# Function: total_returns
+# Takes all possible pairs pairs of the returns between the young and old version of the scientists of interest 
+# and calculates the maximum total return given the young and old returns
+# Return: pair of returns that produce the maximum return over both time period t and t+1
+def total_returns(young_returns_options, old_returns_options):
+
+
+
+def main():
+	# Set these for now
+	young_effort_constant = [0.4, 0.4]
+	old_effort_constant = [0.3, 0.3, 0.3]
+
+	# set this for now: build function for this later
+	possible_young_effort_splits = [[0,4, 0.4], [0, 0.9], [0.9, 0], [0.3, 0.5], \
+									[0.5, 0.3], [0.1, 0.7], [0.7, 0.1], \
+									[0.6, 0.3], [0.3, 0.6]]
+
+	possible_old_effort_splits = 
+
+
+
+if __name__ == "__main__":
+    main()
--- possible_effort_splits.py
+++ possible_effort_splits.py
@@ -2,12 +2,12 @@ import itertools
 
 # Function: all_possible_effort_splits
 # Parameters: 
-# num_of_effort_units - the number of effort units the total K efforts is split into (i.e. 10 means K/10 is one effort unit)
+# size_of_effort_units - the number of effort units the total K efforts is split into (i.e. 10 means K/10 is one effort unit)
 # number_of_ideas - number of ways we want to split the effort units
 # Given a list, gives you all possible combinations of effort 
-# def all_possible_effort_splits(total_effort, num_of_effort_units, num_of_ideas, k):
-# 	effort_unit = float(total_effort)/num_of_effort_units
-# 	rng = list(range(num_of_effort_units+1))*num_of_ideas
+# def all_possible_effort_splits(total_effort, size_of_effort_units, num_of_ideas, k):
+# 	effort_unit = float(total_effort)/size_of_effort_units
+# 	rng = list(range(size_of_effort_units+1))*num_of_ideas
 # 	rng = [round(x * effort_unit, 2) for x in rng]
 # 	effort_splits = set(i for i in itertools.permutations(rng, num_of_ideas) if values_equal(sum(i), total_effort))
 # 	effort_splits = effort_splits.union(i for i in itertools.permutations(rng, num_of_ideas) if values_equal(sum(i), total_effort-k))
@@ -16,62 +16,115 @@ import itertools
 
 
 
-def all_possible_effort_splits():
+def all_possible_effort_splits(total_effort, cost, effort_unit):
 	#options = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-	options = range(11)
+	options = range(0, total_effort+1, effort_unit)
 	list_of_options = []
 	
 
 	for i in range(len(options)):
 		for j in range(len(options)):
 			for k in range(len(options)):
-				if (i+j+k) == 10 or (i+j+k) == 9 or (i+j+k) == 8:
-					idec = i/10.0
-					jdec = j/10.0
-					kdec = k/10.0
-					list_of_options.append((idec, jdec, kdec))
+				x = options[i]+options[j]+options[k]
+				if x == total_effort or x == (total_effort-cost) or x == (total_effort- (2*cost)):
+					idec = options[i]
+					jdec = options[j]
+					kdec = options[k]
+					list_of_options.append([idec, jdec, kdec])
 
 	print len(list_of_options)
 
-	return set(list_of_options)
+	return list_of_options
 					
 
-def remove_impossible_splits(all_effort_splits, total_effort, num_of_effort_units, num_of_ideas, k):
-	splits_to_remove = set()
+def remove_impossible_splits(all_effort_splits, total_effort, size_of_effort_units, num_of_ideas, k):
+	splits_to_remove = []
 	for effort_split in all_effort_splits:
 		# print(effort_split)
 		# print(effort_split[0])
 		# print(sum(effort_split))
 		# print(effort_split[0] != 0.0 and sum(effort_split) == total_effort)
 
-		if values_equal(effort_split[0], total_effort): # Case 1
-			splits_to_remove.add(effort_split)
-		elif not values_equal(effort_split[0], 0.0) and values_equal(sum(effort_split), total_effort):
-		# elif int(1000*effort_split[0]) != int(1000*0.0) and values_equal(sum(effort_split), total_effort):
-			splits_to_remove.add(effort_split)
-		elif values_equal(sum(effort_split), total_effort-2*k) and two_zero_elem(effort_split):
-			splits_to_remove.add(effort_split)
-		elif values_equal(effort_split[0], 0.0) and values_equal(sum(effort_split), total_effort-2*k):
-			splits_to_remove.add(effort_split)
+		# if values_equal(effort_split[0], total_effort): # Case 1
+		# 	splits_to_remove.add(effort_split)
+		# elif not values_equal(effort_split[0], 0) and values_equal(sum(effort_split), total_effort):
+		# # elif int(1000*effort_split[0]) != int(1000*0.0) and values_equal(sum(effort_split), total_effort):
+		# 	splits_to_remove.add(effort_split)
+		# elif values_equal(sum(effort_split), total_effort-2*k) and two_zero_elem(effort_split):
+		# 	splits_to_remove.add(effort_split)
+		# elif values_equal(effort_split[0], 0.0) and values_equal(sum(effort_split), total_effort-2*k):
+		# 	splits_to_remove.add(effort_split)
 
-	all_effort_splits = all_effort_splits.difference(splits_to_remove)
+
+		if effort_split[0] == total_effort:
+			splits_to_remove.append(effort_split)
+		elif effort_split[0] != 0 and sum(effort_split) == total_effort:
+			splits_to_remove.append(effort_split)
+		elif sum(effort_split) == total_effort-2*k and has_n_zero_elems(effort_split, 2):
+			splits_to_remove.append(effort_split)
+		elif effort_split[0] == 0 and sum(effort_split) == total_effort-2*k:
+			splits_to_remove.append(effort_split)
+
+
+	all_effort_splits = remove_from_list(all_effort_splits, splits_to_remove)
+
 	return all_effort_splits
 
+def swap_first_and_last_elems(list_to_swap):
+	temp = list_to_swap[len(list_to_swap)-1]
+	list_to_swap[len(list_to_swap)-1] = list_to_swap[0]
+	list_to_swap[0] = temp
+	return(list_to_swap)
 
-def two_zero_elem(list_of_nums):
+# CAUTION: Call this before you make everything into a decimal to avoid float issues
+def remove_splits_based_on_young_effort_splits(young_split, possible_old_splits, k):
+	# if there is one zero in the splits
+	if has_n_zero_elems(young_split, 0):
+		# remove item if the sum is 1-2k
+		for split in possible_old_splits:
+			if sum(split) == 1-2*k: 
+				possible_old_splits.remove(split)
+	elif young_split[0] == 0:
+		# remove item if the sum is 
+		pass
+	elif young_split[1] == 0:
+		pass
+
+# all possible cases:
+# When young == (0, t)
+# CASE 1: (t-1) and (t+1) != 0
+# CASE 2: (t-1) != 0 and (t+1) == 0
+# CASE 3: (t-1) == 0 and (t+1) != 0
+
+# When young == (t, 0)
+# CASE 1: (t) and (t+1) != 0
+# CASE 2: (t) != 0 and (t+1) == 0
+# CASE 3: (t) == 0 and (t+1) != 0
+
+
+def remove_from_list(original_list, to_remove):
+	for item in to_remove:
+		original_list.remove(item)
+	return original_list
+
+
+def has_n_zero_elems(list_of_nums, num_of_zeroes):
 	zero_elems = [i for i, e in enumerate(list_of_nums) if e == 0]
-	return len(zero_elems) == 2
+	return len(zero_elems) == num_of_zeroes
 
 def values_equal(val1, val2):
 	return int(10*val1) == int(10*val2)
 
 
 
-# def print_sorted_set(unsorted_set):
-# 	sorted_list = list(unsorted_set)
-# 	print(sorted_list)
-# 	sorted_list = sorted_list.sort()
-# 	print(sorted_list)
+def make_decimal(list_to_decimal, places):
+
+	for item in list_to_decimal:
+		for j in range(len(item)):
+			item[j] = item[j] / (10.0 ** places)
+
+	return list_to_decimal
+
 
 def float_equals_int(float_num, int_num):
 	if int(1-float_num) == int_num:
@@ -79,31 +132,39 @@ def float_equals_int(float_num, int_num):
 	return False
 
 def main():
-	k = 0.1
-	total_effort = 1.0
-	num_of_effort_units = 10 #Comment this more thoroughly because unintuitive
-	num_of_ideas = 3
-	# all_effort_splits = all_possible_effort_splits(total_effort, num_of_effort_units, num_of_ideas, k)
+	k = float(raw_input("K? "))
+	total_effort = float(raw_input("Total efforts? "))
+	# size_of_effort_units = float(raw_input("Number of effort units?" ))
+	size_of_effort_units = float(raw_input("Size of effort unit?" ))
+	decimals = int(raw_input("Decimals? "))
+
+	k = int(k*(10**decimals))
+	total_effort = int(total_effort*(10**decimals))
+	size_of_effort_units = int(size_of_effort_units*(10**decimals)) #Comment this more thoroughly because unintuitive
+	num_of_ideas = 2
+	# all_effort_splits = all_possible_effort_splits(total_effort, size_of_effort_units, num_of_ideas, k)
 	# print((0.2, 0.0, 0.4) in all_effort_splits)
 	# print(all_effort_splits)
-	all_effort_splits = all_possible_effort_splits()
+	all_effort_splits = all_possible_effort_splits(total_effort, k, size_of_effort_units)
 
-	for effort_split in all_effort_splits:
-		if values_equal(effort_split[0], 0.6):
-			print effort_split
+	#for effort_split in all_effort_splits:
+		#if values_equal(effort_split[0], 6):
+			#print effort_split
 
-	all_effort_splits = remove_impossible_splits(all_effort_splits, total_effort, num_of_effort_units, num_of_ideas, k)
+	all_effort_splits = remove_impossible_splits(all_effort_splits, total_effort, size_of_effort_units, num_of_ideas, k)
 
+	all_effort_splits = make_decimal(all_effort_splits, decimals)
+	all_effort_splits = swap_first_and_last_elems(all_effort_splits)
 	print(all_effort_splits)
 	print(len(all_effort_splits))
 
-	list_of_interest = []
-	for effort_split in all_effort_splits:
-		if values_equal(effort_split[0], 0.0):
-			list_of_interest.append(effort_split)
+	#list_of_interest = []
+	#for effort_split in all_effort_splits:
+		#if values_equal(effort_split[0], 0):
+			#list_of_interest.append(effort_split)
 
-	print len(list_of_interest)
-	print(list_of_interest)
+	#print len(list_of_interest)
+	#print(list_of_interest)
 	# print_sorted_set(all_effort_splits)
 
 if __name__ == "__main__":
