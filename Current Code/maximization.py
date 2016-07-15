8b2fc71ab7e625b6e7a18199135904d46b943cc6
mzhao94 <miraezhao@gmail.com>
2016-07-15 15:27:39 -0700

    Organized Files

--- Current Code/maximization.py
+++ Current Code/maximization.py
@@ -9,27 +9,6 @@ class TimePeriod(Enum):
 	t = 1
 	tplusone = 2
 
-
-
-# # Function: returns_for_young
-# # Calculate a return for a specific effort split of the maximizing scientist of interest in time period t
-# # Return: a return for that scientist in time period t
-# def returns_for_young(effort_on_tminusone, effort_on_t):
-# 	pass
-
-# # Function: returns_for_old
-# # Calculate the returns for specific effort split of the maximizing scientist of interest in time period t+1
-# # Return: a return for that scientist in time period t+1
-# def returns_for_old(effort_on_tminusone, effort_on_t, effort_on_tplusone):
-# 	pass
-
-# # Function: total_returns
-# # Takes all possible pairs pairs of the returns between the young and old version of the scientists of interest 
-# # and calculates the maximum total return given the young and old returns
-# # Return: pair of returns that produce the maximum return over both time period t and t+1
-# def total_returns(young_returns_options, old_returns_options):
-# 	pass
-
 def return_for_old_young_pair(young_split_constant, old_split_constant, dict_of_pairs):
 	max_return_young_old_pair = None
 	max_return = 0
