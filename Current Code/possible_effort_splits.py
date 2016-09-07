7660bbffc3370263f560ddb88ae4bfd206d558e4
mzhao94 <mzhao94@stanford.edu>
2016-09-07 12:19:35 -0700

    debug

--- Current Code/possible_effort_splits.py
+++ Current Code/possible_effort_splits.py
@@ -182,11 +182,11 @@ def all_possible_young_splits(young_splits, k, total_effort, size_of_effort_unit
 
 def all_possible_old_splits(old_splits, young_split, k_old, k_young, total_effort, size_of_effort_units, decimals):
 	remove_impossible_old_splits(old_splits, total_effort, size_of_effort_units, k_old)
+	remove_old_splits_based_on_young_effort_splits(young_split, old_splits, k_young, total_effort)
+	old_splits = make_decimal(old_splits, decimals)
 	print old_splits
 	print ' '
 	print ' '
-	remove_old_splits_based_on_young_effort_splits(young_split, old_splits, k_young, total_effort)
-	old_splits = make_decimal(old_splits, decimals)
 	return old_splits
 
 
